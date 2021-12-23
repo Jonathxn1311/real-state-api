d3c663d3f3d30765026468c7a8ca97d466c5c963
Jonathan <jonathanalmengot1311@gmail.com>
2021-12-23 11:53:10 -0400

    Configuraciones con el Model y la conexion con MongoDB

--- src/models/property.js
+++ src/models/property.js
@@ -1,5 +1,6 @@
 const db = require("../connection.js")
 const mongoose = require("mongoose")
+mongoose.connect('mongodb://localhost:27017/real-state')
 const { Schema } = mongose
 
 const propertySchema = new Schema({
