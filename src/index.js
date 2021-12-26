7191f5766b35fb04afc9da1dc1e59309c22f5300
Jonathan <jonathanalmengot1311@gmail.com>
2021-12-25 21:29:14 -0400

    Se agrega funcionalidad para que cada solicitud imprima en la consola lo realizado

--- src/index.js
+++ src/index.js
@@ -12,6 +12,10 @@ app.get("/", (req, res) => {
 })
 
 app.get("/properties", async (req, res) => {
+    console.log("Se solicito ver todas las propiedades")
+    console.log({
+        properties: await Property.find({})
+    })
     res.json({
         properties: await Property.find({})
     })
@@ -24,6 +28,11 @@ app.get("/property/:id", (req, res) => {
             throw new Error('Error encontrando la propiedad')
         }
         res.json({
+            property
+        })
+        console.log("Request a /property/:id")
+        console.log("Propiedad encontranda")
+        console.log({
             property
         })
     })
@@ -41,16 +50,32 @@ app.post("/property", (req, res) => {
         if(err){
             throw new Error('Error creating the new property')
         } 
+        const property = Property.findOne({ name: req.query.name })
         res.end('Property inserted correctly')
+        console.log("Request post a /property")
+        console.log("Propiedad insertada: ")
+        console.log({
+            property
+        })
     })
 })
 
 app.delete('/property/:id', (req, res) => {
+    const property = Property.findOne({ _id: req.params.id })
+
+    console.log("Request delete a /property/:id")
+    console.log("Propiedad a eliminar: ")
+    console.log({
+        property
+    })
+
     Property.deleteOne({ _id: req.params.id }, (err) => {
         if (err){
             res.end('An error ocurred while deleting the property')
+            console.log('An error ocurred while deleting the property')
         } else {
             res.end('Property deleted correctly')
+            console.log('Property deleted correctly')
         }
     })
 })
